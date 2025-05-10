6b9d50f23347e76e5f2881906ae247cd00a84250
Amber <amber@transcental.dev>
2025-05-10 19:14:50 +0100

    fix paths

--- Sources/hello/Controllers/ChallengeController.swift
+++ Sources/hello/Controllers/ChallengeController.swift
@@ -5,19 +5,19 @@ struct ChallengeController: RouteCollection {
     func boot(routes: any RoutesBuilder) throws {
         let challenge = routes.grouped("challenges")
         challenge.get(use: index)
-        challenge.get("uk-transport", use: ukTransport)
+        challenge.get("uk-operators", use: ukOperators)
     }
     
     func index(req: Request) async throws -> View {
         try await req.view.render("challenges/index")
     }
     
-    func ukTransport(_ req: Request) async throws -> View {
+    func ukOperators(_ req: Request) async throws -> View {
         let operators = try await Operator.query(on: req.db).filter(\.$operatesTrains == true).all()
         
         return try await req.view
             .render(
-                "challenges/uk-transport",
+                "challenges/uk-operators",
                 ["operators": operators.sorted(by: { $0.name < $1.name })]
             )
     }
